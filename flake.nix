f79d3851c761e105aef6299298950de1444e5d16
Matteo Gassend <mgassend@gmail.com>
2024-10-05 12:31:15 +0200

    add: flake.nix

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.go
+          ];
+        };
+        packages.default = pkgs.buildGoModule {
+          pname = "libsqltui";
+          version = "0.1.0";
+          src = ./.;
+          module = "github.com/matfire/libsqltui";
+          vendorHash = "sha256-lzMBz4HnISzHU47CM8M/xrs1UiCF+GcsksJoX5H0tSg=";
+        };
+ 
+      });
+}
