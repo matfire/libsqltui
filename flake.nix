243cfd2135c4f0405a75aa2c06ae83bf27d9e747
Matteo Gassend <mgassend@gmail.com>
2024-10-06 12:13:37 +0200

    fix: flake used only for devShell

--- flake.nix
+++ flake.nix
@@ -3,25 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            pkgs.go
-          ];
-        };
-        packages.default = pkgs.buildGoModule {
-          pname = "libsqltui";
-          version = "0.1.0";
-          src = ./.;
-          module = "github.com/matfire/libsqltui";
-          vendorHash = "sha256-cVkroKJlU+s9dIRuNSbKAk0evpwYTSoG6ZvtQzdRUaE=";
-        };
- 
-      });
+  outputs = {self, nixpkgs}:
+    let 
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+          buildInputs = [pkgs.go];
+      };
+    };
 }
