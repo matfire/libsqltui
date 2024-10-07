21915760b05f61fbe3359ae8c60a026fccb61d13
Matteo Gassend <mgassend@gmail.com>
2024-10-07 14:04:03 +0200

    add: sqld to flake devShell

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       devShells.${system}.default = pkgs.mkShell {
-          buildInputs = [pkgs.go pkgs.goreleaser];
+          buildInputs = [pkgs.go pkgs.goreleaser pkgs.sqld];
       };
     };
 }
